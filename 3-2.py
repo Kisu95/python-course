fcd8750490db6dec9e6763119b10701f46095050
kisu95 <mastalska.agata@gmail.com>
2019-04-13 21:34:39 +0200

    Update 3-2.py
    add classes & input

--- 3-2.py
+++ 3-2.py
@@ -1,59 +1,64 @@
 #2 Write a function which takes sets of parameters of two figures and compares their fields. (4p)
 # The exemplary input is [[Circle, 4], [Rhombus, 2, 4]]
 # Expected output would be 'The first figure (Circle) has larger field'
-from cs50 import get_float, get_string
 from math import pi
 
-def countField(field, x, y=0):
-    if (field == "circle"):
-        return 2*pi*x
-    if (field == "rectangle"):
-        return x*y
-    if (field == "triangle" or field == "rhombus"):
-        return (x*y)/2
+class Error(Exception):
+   """Base class for other exceptions"""
+   pass
+class Zero(Error):
+   pass
+class Figures(Error):
+   pass
+class Sign(Error):
+   pass
 
-figures = []
-fields = []
+def Field(fig):
+    try:
+        figName = fig[0].lower()
+        x = float(fig[1])
+        if len(fig) > 2:
+            y = float(fig[2])
+        else:
+            y = 1
+        assert(x > 0 and y > 0)
+    except AssertionError:
+        print("X and Y should be greater than 0")
+    fields = {"circle": 2 * pi * x,
+            "rectangle": x * y,
+            "triangle": 0.5 * x * y,
+            "rhombus": x * y * 0.5}
+    field = fields[figName]
+    return field
 
-for i in range(2):
-    while True:
-        try:
-            fieldType = get_string(
-                "Enter name of figure. Choose one of these: circle/rectangle/triangle/rhombus:  ")
-            fieldType = fieldType.lower()
-            assert(fieldType == "circle" or fieldType ==
-                "rectangle" or fieldType == "triangle" or fieldType == "rhombus")
-            break
-        except AssertionError:
-            print("Choose one of these figures: circle/rectangle/triangle/rhombus ")
-
-    while True:
-        try:
-            x = get_float("Enter x: ")
-            assert(x > 0)
-            break
-        except AssertionError:
-            print("Number should be greater than 0")
-
-    if (fieldType != "circle"):
-        while True:
-            try:
-                y = get_float("Enter y: ")
-                assert(y > 0)
-                break
-            except AssertionError:
-                print("Number should be greater than 0")
-        figures.append([fieldType, x, y])
-        fields.append(countField(fieldType, x, y))
+def compareField(fields):
+    stField = Field(fields[0])
+    nnField = Field(fields[1])
+    if stField > nnField:
+        return print("The first figure (" + fields[0][0] + ") has larger field.")
+    elif stField == nnField:
+        return print("Figures have the same fields.")
     else:
-        figures.append([fieldType, x])
-        fields.append(countField(fieldType, x))
+        return print("The second figure (" + fields[1][0] + ") has larger field.")
 
-#print(figures)
-
-if (fields[0] > fields[1]):
-    print("The first figure (" + figures[0][0] + ") has larger field.")
-elif (fields[1] > fields[0]):
-    print("The second figure (" + figures[1][0] + ") has larger field.")
-else:
-    print("Figures have the same fields.")
+try:
+    inputs = [['rectangle', 2, 8], ['rectangle', 7, 2]]
+    if len(inputs) != 2:
+       raise Figures
+    if ((inputs[0][0]).lower() == "circle" and len(inputs[0]) != 2) or ((inputs[0][0]).lower() != "circle" and len(inputs[0])):
+       raise Sign
+    if (inputs[1][0]).lower() == "circle" and len(inputs[1]) != 2 or (inputs[0][0]).lower() != "circle" and len(inputs[1]) != 3:
+       raise Sign
+    compareField(inputs)
+except ValueError:
+    print("Enter correct number.")
+except Zero:
+    print("Length can not be negative")
+except KeyError:
+    print("Choose one of these figures: circle/rectangle/triangle/rhombus ")
+except Sign:
+    print("Check your input")
+except IndexError:
+    print("wrong number of characters was entered")
+except Figures:
+    print("wrong amount of figures")
